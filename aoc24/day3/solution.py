ee8bf9d35530eb7877ad2b28d5d26efcd5089ae4
yukova31 <yukova.31@gmail.com>
2024-12-03 20:28:48 +0000

    Added a solution using regular expressions

--- aoc24/day3/solution.py
+++ aoc24/day3/solution.py
@@ -1,3 +1,6 @@
+import re
+
+
 def parse_data(lines: list[str]):
     # takes lines of the input, processes data into data structures fir for the task
     string = ""
@@ -7,80 +10,57 @@ def parse_data(lines: list[str]):
 
 
 def solve_part1(command: str, dummy):
-    # can also do regular expressions, but it's much more fun
-    numerics = {chr(a) for a in range(ord("0"), ord("9") + 1)}
-    template = "mul("
-    ans, i = 0, 0
-    matched_from_template = 0
-    while i < len(command):
-        matched_from_template = (
-            matched_from_template + 1
-            if command[i] == template[matched_from_template]
-            else 0
-        )
-        i += 1
-        if matched_from_template < len(template):
-            continue
-        num1_str = ""
-        while i < len(command) and command[i] in numerics:
-            num1_str += command[i]
-            i += 1
-        if len(num1_str) == 0 or i == len(command) or command[i] != ",":
-            matched_from_template = 0
-            continue
-        num1 = int(num1_str)
-        i += 1
-
-        num2_str = ""
-        while i < len(command) and command[i] in numerics:
-            num2_str += command[i]
-            i += 1
-        if len(num2_str) == 0 or i == len(command) or command[i] != ")":
-            matched_from_template = 0
-            continue
-        num2 = int(num2_str)
-        ans += num1 * num2
-        matched_from_template = 0
-
+    templates = [
+        re.compile(r"mul\(\d+,\d+\)"),
+    ]
+    number = re.compile(r"\d+")
+    ans = 0
+    while len(command) > 0:
+        best_match = None
+        first_found_j = None
+        for j, t in enumerate(templates):
+            match = t.search(command)
+            if match and (best_match is None or match.start() < best_match.start()):
+                best_match = match
+                first_found_j = j
+        if first_found_j is None:
+            break
+        command = command[best_match.end() :]
+        numbers = [int(x) for x in number.findall(best_match.group())]
+        ans += numbers[0] * numbers[1]
     return ans
 
 
 def solve_part2(command: str, dummy):
     # even more regular expressions here
 
-    numerics = {chr(a) for a in range(ord("0"), ord("9") + 1)}
-    templates = ["mul(", "do()", "don't()"]
-    matches = [0, 0, 0]
-    ans, i = 0, 0
+    templates = [
+        re.compile(r"do\(\)"),
+        re.compile(r"don't\(\)"),
+        re.compile(r"mul\(\d+,\d+\)"),
+    ]
+    number = re.compile(r"\d+")
+    ans = 0
     enabled = True
-    while i < len(command):
+    while len(command) > 0:
+        best_match = None
+        first_found_j = None
         for j, t in enumerate(templates):
-            matches[j] = matches[j] + 1 if command[i] == t[matches[j]] else 0
-        i += 1
-        for j, t in enumerate(templates):
-            if matches[j] == len(t):
-                matches[j] = 0
-                if j == 1:
-                    enabled = True
-                elif j == 2:
-                    enabled = False
-                elif enabled:
-                    num1_str = ""
-                    while i < len(command) and command[i] in numerics:
-                        num1_str += command[i]
-                        i += 1
-                    if len(num1_str) == 0 or i == len(command) or command[i] != ",":
-                        continue
-                    num1 = int(num1_str)
-                    i += 1
-
-                    num2_str = ""
-                    while i < len(command) and command[i] in numerics:
-                        num2_str += command[i]
-                        i += 1
-                    if len(num2_str) == 0 or i == len(command) or command[i] != ")":
-                        continue
-                    num2 = int(num2_str)
-                    ans += num1 * num2
-
+            match = t.search(command)
+            if match and (best_match is None or match.start() < best_match.start()):
+                best_match = match
+                first_found_j = j
+        if first_found_j is None:
+            break
+        command = command[best_match.end() :]
+        match first_found_j:
+            case 0:
+                enabled = True
+            case 1:
+                enabled = False
+            case 2:
+                if not enabled:
+                    continue
+                numbers = [int(x) for x in number.findall(best_match.group())]
+                ans += numbers[0] * numbers[1]
     return ans
---
+++ aoc24/day4/solution.py
@@ -0,0 +1,16 @@
+def parse_data(lines: list[str]):
+    # takes lines of the input, processes data into data structures fir for the task
+    string = ""
+    for line in lines:
+        string += line
+    return string, None
+
+
+def solve_part1(command: str, dummy):
+
+    return
+
+
+def solve_part2(command: str, dummy):
+
+    return
